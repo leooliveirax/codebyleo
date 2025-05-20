ee07bd1477c55ebc0e5c781348067f33d99da432
leonardoXoliveira <xleonardo.x.oliveirax@gmail.com>
2025-05-20 20:28:44 -0300

    feat: enhance writings layout with gradient effect for improved visual experience; add bottom padding to welcome page for better spacing

--- src/app/writings/layout.tsx
+++ src/app/writings/layout.tsx
@@ -1,3 +1,15 @@
 export default function WritingsLayout({ children }: { children: React.ReactNode }) {
-  return <div className="mx-auto max-w-screen-sm space-y-6">{children}</div>;
+  return (
+    <>
+      <div className="mx-auto max-w-screen-sm space-y-6">{children}</div>
+
+      {/* Gradient to hide the content when scrolling */}
+      <div
+        className="pointer-events-none fixed bottom-0 left-0 h-20 w-full backdrop-blur-xs"
+        style={{
+          maskImage: "linear-gradient(to top, black 25%, transparent)",
+        }}
+      />
+    </>
+  );
 }
